bd4c5faa9afaf0650100b6849e5dd19b9e2d6bb5
elallali <yassine.elallali@universign.com>
2022-08-03 14:36:00 +0200

    add test

--- src/test/java/com/ensim/muremagique/services/CodeServiceTest.java
+++ src/test/java/com/ensim/muremagique/services/CodeServiceTest.java
@@ -2,6 +2,7 @@ package com.ensim.muremagique.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 
@@ -13,6 +14,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mockito.Mockito;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +37,7 @@ public class CodeServiceTest
 	}
 
 	@Test
-	public void uploadCodeShouldSucceed()
+	public void addCodeShouldSucceed()
 	{
 		// Given
 		MultipartFile file = new MockMultipartFile("test", "test", null, new byte[]{});
@@ -49,4 +54,58 @@ public class CodeServiceTest
 		assert(actual.getOrder()).equals(1);
 		assert(actual.getPath()).equals("test");
 	}
+
+	@Test
+	public void addCodeShouldFailWhenFileAlreadyExist()
+	{
+		// Given
+
+		MultipartFile file = new MockMultipartFile("test", "test", null, new byte[]{});
+
+		doNothing().when(storageService).store(any());
+		given(codeRepository.count()).willReturn(0l);
+		given(codeRepository.save(any(Code.class))).willReturn(new Code("dummy", 0));
+		given(codeRepository.existsByPath("test")).willReturn(true);
+
+		// Then
+
+		assertThrows(BusinessException.class, () -> {
+			codeService.addCode(file);
+		});
+	}
+
+	@Test
+	public void popCodeShouldSucceed()
+	{
+		// Given
+		List<Code> codes = List.of(new Code("test1", 1), new Code("test2", 2));
+
+		given(codeRepository.findByOrderByOrderAsc()).willReturn(codes);
+
+		// When
+
+		Code code = codeService.popCode();
+
+		// Then
+
+		assert(code.getPath()).equals("test1");
+		assert(code.getOrder()).equals(0);
+
+	}
+
+	@Test
+	public void popCodeShouldFailWhenNoCodeExist()
+	{
+		// Given
+		List<Code> codes = new ArrayList<>();
+
+		given(codeRepository.findByOrderByOrderAsc()).willReturn(codes);
+
+		// Then
+
+		assertThrows(BusinessException.class, () -> {
+			codeService.popCode();
+		});
+	}
+
 }
